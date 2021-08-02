aa00c60b485595cd946afc46afd650a052d022b4
Gabriel Coimbra <gabao.coimbra@gmail.com>
2021-08-02 14:43:31 -0300

    Reject e then promisse pt  3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,8 +14,16 @@ export class HomeComponent implements OnInit {
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    this.ofertas = this.ofertasService.getOfertas()
+    // this.ofertas = this.ofertasService.getOfertas()
     // console.log(this.ofertas)
+    this.ofertasService.getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas
+      },
+        (param: any) => { console.log(param) }
+      )
+
+
   }
 
 }
